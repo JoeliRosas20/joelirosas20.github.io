15863d32ccf7d3daf6309d81878dcc3af3904f5d
Joel Rosas <joeli.rosas20@gmail.com>
2021-03-01 17:29:46 -0500

    modified App.js to say Joel

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        My name is Joel and I'm going to master React
         <a
           className="App-link"
           href="https://reactjs.org"
