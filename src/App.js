2f3fb0a025845b44a4e52cf9528a4b6e5f8c6801
Joel Rosas <joeli.rosas20@gmail.com>
2021-05-14 15:48:07 -0400

    Had to

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import Site from './Site';
 import './App.css';
 import React from 'react';
 
--- src/Site.js
+++ src/Site.js
@@ -1,16 +1,10 @@
 import React from 'react';
-import Home from '.components/Home';
-import AboutMe from '.components/AboutMe';
-import Projects from '.components/Projects';
-import Contact from '.components/Contact';
+
 
 function Site() {
     return (
         <div>
-            <Home/>
-            <AboutMe/>
-            <Projects/>
-            <Contact/>
+            
         </div>
     )
 }
