b95ea2004bf37a4b99922a81cb30d07fe67aa94b
Joel Rosas <joeli.rosas20@gmail.com>
2021-05-14 13:17:55 -0400

    Imported files to site and made edit to About Me

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -3,7 +3,8 @@ import React from 'react'
 function aboutMe() {
     return (
         <div>
-            About Me
+            <h1>About Me</h1>
+            <p>My name is Joel and I'm going to master React!!!</p>
         </div>
     )
 }
---
+++ src/components/site.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import home from '.components/home'
+import aboutMe from '.components/aboutMe'
+import projects from '.components/projects'
+import contacts from '.components/contacts'
+
+function site() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default site
