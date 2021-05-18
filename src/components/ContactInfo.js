242aa5a4a758d8794c8030948a7d6cb1fef35827
Joel Rosas <joeli.rosas20@gmail.com>
2021-05-18 15:20:40 -0400

    Added the textarea for the message

--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -10,13 +10,17 @@ function ContactInfo() {
             <a href="https://www.linkedin.com/in/joel-rosas-50b653170/" target="_blank" rel="noopener noreferrer"> Linkedin</a>
             <form>
                 <label htmlFor="name">Name</label>
-                <input
-                type="text"
-                id="name"/>
+                    <input
+                    type="text"
+                    id="name"/>
+                    <br></br>
                 <label htmlFor="email">Email</label>
-                <input
-                type="text"
-                id="email"/>
+                    <input
+                    type="text"
+                    id="email"/>
+                    <br></br>
+                <label htmlFor="message">Send me a message!</label>
+                    <textarea rows="10" cols="30" id="message"/>
             </form>
         </div>
     )
