025d6415685348f00359a03817cb9ee90e579788
Joel Rosas <joeli.rosas20@gmail.com>
2021-05-18 14:53:37 -0400

    Added the label and input to put their name

--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -8,6 +8,12 @@ function ContactInfo() {
             Gmail: joeli.rosas20@gmail.com
             <br></br> 
             <a href="https://www.linkedin.com/in/joel-rosas-50b653170/" target="_blank" rel="noopener noreferrer"> Linkedin</a>
+            <form>
+                <label htmlFor="name">Name</label>
+                <input
+                type="text"
+                id="name"/>
+            </form>
         </div>
     )
 }
