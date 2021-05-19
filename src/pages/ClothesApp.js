c928ddbcb5db1ebeaacc9f66885747189e3aa499
Joel Rosas <joeli.rosas20@gmail.com>
2021-05-19 18:37:51 -0400

    Added the links in SpaceGame and ClothesApp to get back to the projects page

--- src/pages/ClothesApp.js
+++ src/pages/ClothesApp.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import {Link} from "react-router-dom"
 
 function ClothesApp() {
     return (
         <div>
             The clothes App
+            <br></br>
+            <Link to="/projects">Projects</Link>
         </div>
     )
 }
--- src/pages/SpaceGame.js
+++ src/pages/SpaceGame.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import {Link} from "react-router-dom"
 
 function SpaceGame() {
     return (
         <div>
             The Space Game
+            <br></br>
+            <Link to="/projects">Projects</Link>
         </div>
     )
 }
