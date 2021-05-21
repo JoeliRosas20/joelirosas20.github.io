352e4384f04f7936ba3e8f4d86f31645bcc9d935
Joel Rosas <joeli.rosas20@gmail.com>
2021-05-21 18:23:14 -0400

    Implemented NavLink to Home String to lead us to home page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,7 +7,9 @@ function NavBar() {
             <div>
                 <ul>
                     <li>
-                        Home
+                        <NavLink to = "/">
+                            Home
+                        </NavLink>
                     </li>
                     <li>
                         About
